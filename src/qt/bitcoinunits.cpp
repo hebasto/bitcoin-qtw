bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

qt: Use QVariant instead of int for BitcoinUnit in QSettings
This change improves type safety.

src/qt/bitcoinunits.cpp
@@ -248,3 +248,39 @@ CAmount BitcoinUnits::maxMoney()
 {
     return MAX_MONEY;
 }
+
+namespace {
+qint8 ToQuint8(BitcoinUnit unit)
+{
+    switch (unit) {
+    case BitcoinUnits::BTC: return 0;
+    case BitcoinUnits::mBTC: return 1;
+    case BitcoinUnits::uBTC: return 2;
+    case BitcoinUnits::SAT: return 3;
+    } // no default case, so the compiler can warn about missing cases
+    assert(false);
+}
+
+BitcoinUnit FromQuint8(qint8 num)
+{
+    switch (num) {
+    case 3: return BitcoinUnits::SAT;
+    case 2: return BitcoinUnits::uBTC;
+    case 1: return BitcoinUnits::mBTC;
+    default: return BitcoinUnits::BTC;
+    }
+}
+} // namespace
+
+QDataStream& operator<<(QDataStream& out, const BitcoinUnit& unit)
+{
+    return out << ToQuint8(unit);
+}
+
+QDataStream& operator>>(QDataStream& in, BitcoinUnit& unit)
+{
+    quint8 input;
+    in >> input;
+    unit = FromQuint8(input);
+    return in;
+}

hebasto/bitcoin-qtw

src/qt/bitcoinunits.cpp
@@ -250,3 +250,40 @@ CAmount BitcoinUnits::maxMoney()
 {
     return MAX_MONEY;
 }
+
+namespace {
+qint8 ToQint8(BitcoinUnit unit)
+{
+    switch (unit) {
+    case BitcoinUnits::BTC: return 0;
+    case BitcoinUnits::mBTC: return 1;
+    case BitcoinUnits::uBTC: return 2;
+    case BitcoinUnits::SAT: return 3;
+    } // no default case, so the compiler can warn about missing cases
+    assert(false);
+}
+
+BitcoinUnit FromQint8(qint8 num)
+{
+    switch (num) {
+    case 0: return BitcoinUnits::BTC;
+    case 1: return BitcoinUnits::mBTC;
+    case 2: return BitcoinUnits::uBTC;
+    case 3: return BitcoinUnits::SAT;
+    }
+    assert(false);
+}
+} // namespace
+
+QDataStream& operator<<(QDataStream& out, const BitcoinUnit& unit)
+{
+    return out << ToQint8(unit);
+}
+
+QDataStream& operator>>(QDataStream& in, BitcoinUnit& unit)
+{
+    qint8 input;
+    in >> input;
+    unit = FromQint8(input);
+    return in;
+}